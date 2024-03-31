3d69409d224b7852b754d3e3ef4f495901a79f64
Sushantabharat <ifssushanta1@gmail.com>
2024-04-01 00:01:41 +0530

    slug added

--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -7,21 +7,21 @@ export default function ProductItem({ product }: { product: Product }) {
   return (
     <div className="card bg-base-300 shadow-xl mb-4">
       <figure>
-        <Link href={'/product/${product.slug}'}>
+        <Link href={`/product/${product.slug}`}>
           <Image
             src={product.image}
             alt={product.name}
-            width={200}
-            height={200}
+            width={300}
+            height={300}
             className="object-cover h-64 w-full"
           />
         </Link>
       </figure>
       <div className="card-body">
-        <Link href={'/product/${product.slug}'}>
-          <h3 className="text-xl font-bold">{product.name}</h3>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="card-title font-normal">{product.name}</h2>
         </Link>
-        <p className="card-text">{product.description}</p>
+        <p className="mb-2">{product.brand}</p>
         <div className="card-actions flex items-center justify-between">
           <span className="text-2xl">${product.price}</span>
         </div>
